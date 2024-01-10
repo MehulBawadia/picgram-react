6cee274344f04bd314f5055b95d266e7a2cc204d
Mehul Bawadia <mehulbawadia@gmail.com>
2024-01-10 16:28:56 +0530

    Added the registration form fields

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-    return <div>AuthLayout</div>;
+    const isAuthenticated = false;
+
+    return (
+        <>
+            {isAuthenticated ? (
+                <Navigate to="/" />
+            ) : (
+                <>
+                    <section className="flex flex-1 flex-col justify-center items-center py-10">
+                        <Outlet />
+                    </section>
+
+                    <img
+                        src="/assets/images/side-img.svg"
+                        alt="Side Image"
+                        className="hidden xl:block h-screen w-1/2 object-cover"
+                    />
+                </>
+            )}
+        </>
+    );
 };
 
 export default AuthLayout;
