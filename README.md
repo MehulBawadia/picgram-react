6a75ade9835412660736ddcd59bcbc51a698683e
Mehul Bawadia <mehulbawadia@gmail.com>
2024-01-10 13:56:17 +0530

    Initial set up

---
+++ README.md
@@ -0,0 +1,3 @@
+## SnapGram
+
+This is a learning tutorial to build Instagram like app which is built using React & TypeScript.
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./globals.css";
+
+const App = () => {
+    return <h1 class="underline">Hello World</h1>;
+};
+
+export default App;
