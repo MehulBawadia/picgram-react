94decfff6c6918a9a6f8a7ac40ab65153ed4d2e4
Mehul Bawadia <mehulbawadia@gmail.com>
2024-01-10 14:30:37 +0530

    Added layout files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import Login from "./_auth/forms/Login";
+import Register from "./_auth/forms/Register";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 import "./globals.css";
 
 const App = () => {
-    return <h1 class="underline">Hello World</h1>;
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* Public Routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Route>
+
+                {/* Private Routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    );
 };
 
 export default App;
