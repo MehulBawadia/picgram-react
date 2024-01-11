a81cac6fd80f4b7b702ba5218f45c40b3d9ef145
Mehul Bawadia <mehulbawadia@gmail.com>
2024-01-11 18:34:58 +0530

    Added the sidebar pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import AuthLayout from "./_auth/AuthLayout";
 import Login from "./_auth/forms/Login";
 import Register from "./_auth/forms/Register";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import {
+    AllUsers,
+    CreatePost,
+    EditPost,
+    Explore,
+    Home,
+    PostDetails,
+    Profile,
+    Saved,
+    UpdateProfile,
+} from "@/_root/pages";
 
 const App = () => {
     return (
@@ -20,6 +30,17 @@ const App = () => {
                 {/* Private Routes */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/saved" element={<Saved />} />
+                    <Route path="/all-users" element={<AllUsers />} />
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:id" element={<EditPost />} />
+                    <Route path="/posts/:id" element={<PostDetails />} />
+                    <Route path="/profile/:id" element={<Profile />} />
+                    <Route
+                        path="/update-profile/:id"
+                        element={<UpdateProfile />}
+                    />
                 </Route>
             </Routes>
 
